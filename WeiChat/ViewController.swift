0c1effa4df2b4b1d2b705c738685d0e4a213549a
Liuming <liuming_cn@qq.com>
2018-10-28 22:43:30 +0800

    Initial Commit

---
+++ WeiChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeiChat
+//
+//  Created by 刘铭 on 2018/10/28.
+//  Copyright © 2018 刘铭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
